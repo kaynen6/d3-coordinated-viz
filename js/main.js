f07aed2b0b37d3c0d1e5873892a814d38fe36fa9
Kayne Neigherbauer <kayne.n@gmail.com>
2017-04-03 17:24:22 -0500

    still working module 9.

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@
     //set up choropleth map
     function setMap(){
         //map frame dimensions
-        var width = 960,
-            height = 460;
+        var width = window.innerWidth * 0.5,
+            height = 500;
 
         //create new svg container
         var map = d3.select("body")
@@ -27,7 +27,7 @@
             .center([0,44.75])
             .rotate([90,0,0])
             .parallels([43.5,45.5])
-            .scale(5000)
+            .scale(6000)
             .translate([width / 2, height / 2]);
 
         var path = d3.geoPath() //generator for projection
@@ -62,9 +62,12 @@
             
             //create the color scale
             var colorScale = makeColorScale(csvData);
+            
             //add enumeration units to map
             setEnumerationUnits(wisCounties,map,path, colorScale);
             
+            //add coordinated visualization to the map
+            setChart(csvData, colorScale);
             
         };
         
@@ -106,7 +109,7 @@
                 })
                 .attr("d",path)
                 .style("fill", function(d){
-                    return colorScale(d.properties[expressed]);
+                    return choropleth(d.properties, colorScale);
                 });
         };
         
@@ -145,6 +148,59 @@
             
             return colorScale;
                         
+        };
+        
+        //function to test for data value and return color
+        function choropleth(props, colorScale){
+            //make sure attribute value is a number
+            var val = parseFloat(props[expressed]);
+            //if attribute value exists, assign a color; otherwise assign gray
+            if (typeof val == 'number' && !isNaN(val)){
+                return colorScale(val);
+            } else {
+                return "#CCC";
+            };
+        };
+        
+        //function to create coordinated chart
+        function setChart(csvData, colorScale){
+            //chart frame dimensions
+            var chartWidth = window.innerWidth * 0.425, 
+                chartHeight = 500;
+            
+            //create second svg element to hold the chart
+            var chart = d3.select("body")
+                .append("svg")
+                .attr("width", chartWidth)
+                .attr("height", chartHeight)
+                .attr("class", "chart");
+            
+            //create a scale to size bars proportionally to frame
+            var yScale = d3.scaleLinear()
+                .range([0, chartHeight])
+                .domain([0, 105]);
+            
+            //set bars for each county
+            var bars = chart.selectAll(".bars")
+                .data(csvData)
+                .enter()
+                .append("rect")
+                .attr("class",function (d){
+                    return "bars " + d.GEO_id2;
+                })
+                .attr("width", chartWidth / csvData.length -1)
+                .attr("x", function(d, i){
+                    return i *(chartWidth / csvData.length)
+                })
+                .attr("height", function(d){
+                    return yScale(parseFloat(d[expressed]));
+                })
+                .attr("y", function(d){
+                    return chartHeight - yScale(parseFloat(d[expressed]));
+                })
+                .style("fill", function(d){
+                    return choropleth(d, colorScale);
+                });
         };
 
     };
